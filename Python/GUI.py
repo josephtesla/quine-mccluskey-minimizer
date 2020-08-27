9f8a8d2141e2481122f024c38dd767e632003fbe
Joseph <kstdeveloper@gmail.com>
2020-08-27 15:59:54 +0000

    clean

--- Python/GUI.py
+++ Python/GUI.py
@@ -6,28 +6,19 @@ class GUI:
 	def __init__(self):
 		window = Tk() # Create a window
 		window.title("Quine-McKluskey Minimizer") # Set title																		#Window
-
 		Label(window, text = "ENTER MINTERMS (seperated by comma e.g 3,4,5...): ").grid(row = 1, column = 1, sticky = W)    #Labels
 		Label(window, text = "ENTER NUMBER OF VARIABLES (e.g 4):").grid(row = 3, column = 1, sticky = W)
 		Label(window, text = ".").grid(row = 2, column = 1, sticky = W)
 		Label(window, text = ".").grid(row = 5, column = 1, sticky = W)
 		Label(window, text = ".").grid(row = 4, column = 1, sticky = W)
 		Label(window, text = "MINIMIZED EXPRESSION: ").grid(row = 5, column = 1, sticky = W)
-
-
 		self.mintermsVar = StringVar()
 		Entry(window, textvariable=self.mintermsVar,justify=RIGHT).grid(row=1,column=3)
-
 		self.variablesVar = StringVar()
 		Entry(window, textvariable=self.variablesVar,justify=RIGHT).grid(row=3,column=3)
-
-
 		self.expressionVar = StringVar()
 		lblTotalPayment = Label(window, textvariable = self.expressionVar).grid(row = 5, column = 3, sticky=E)
-	
-
 		btMinimize = Button(window, text = "Minimize", command=self.minimize).grid(row = 6, column = 2, sticky=E)
-	 	
 		window.mainloop()
 
 	def minimize(self):
@@ -36,12 +27,10 @@ class GUI:
 
 			try:
 				func = QM_Minimizer(minterms, no_of_vars).minimize()
-
 				self.expressionVar.set(func)
 
 			except Exception as InvalidInputException:
 
 				self.expressionVar.set("Looks like your input is invalid")
-
 				raise InvalidInputException
 
--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,9 @@
 """
   DRIVER PROGRAM
-
 """
 
 from qm_minimizer import QM_Minimizer
 from GUI import GUI
-
 
 def main():
 	print("|-----------------------------------------------|")
@@ -22,43 +20,29 @@ def main():
 	print("|																				|")
 	print("|-----------------------------------------------|")
 
-
 	env = input("Enter here: ")
-
 	while env not in ['1', '2']:
 		print("!INVALID INPUT!")
 		env = input("Enter here: ")
-
 
 	if env == "1":
 		GUI() #RUN GUI instance
 
 	elif env == "2":
 		print("\n|-------------- CONSOLE MODE -------------| \n")
-
 		invalid = True
-
-
 		while invalid:
 			try:
-
 				print("|-----ENTER MINTERMS SEPERATED BY COMMA (e.g 3,2,5,12...) ------|")
 				minterms = input("minterms: ")
-
 				print("\n\n|-----ENTER NUMBER OF VARIABLES (e.g 4) ------|")
 				no_of_vars = int(input("no of variables: "))
-
 				func = QM_Minimizer(minterms, no_of_vars).minimize()
-
 				print("|------ MINIMIZED EXPRESSION -------| \n")
 				print("\t " + func)
-
 				invalid = False
 
 			except Exception as InvalidInputException:
 				print("\n\n!----- ERROR: INVALID FUNCTION INPUT------|\n\n")
-				
 				invalid = True
-				
-
 main()
